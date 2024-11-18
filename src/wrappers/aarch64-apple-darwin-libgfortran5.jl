c9fdc45328bce8080ff427a8250a7b20846ec425
jlbuild <juliabuildbot@gmail.com>
2024-11-18 03:21:23 +0000

    Arpack32_jll build 3.9.1+1

--- src/wrappers/aarch64-apple-darwin-libgfortran5.jl
+++ src/wrappers/aarch64-apple-darwin-libgfortran5.jl
@@ -2,11 +2,11 @@
 export libarpack32
 
 using CompilerSupportLibraries_jll
-using OpenBLAS32_jll
+using libblastrampoline_jll
 JLLWrappers.@generate_wrapper_header("Arpack32")
 JLLWrappers.@declare_library_product(libarpack32, "@rpath/libarpack.2.dylib")
 function __init__()
-    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, OpenBLAS32_jll)
+    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, libblastrampoline_jll)
     JLLWrappers.@init_library_product(
         libarpack32,
         "lib/libarpack.2.1.0.dylib",
--- src/wrappers/armv6l-linux-gnueabihf-libgfortran3.jl
+++ src/wrappers/armv6l-linux-gnueabihf-libgfortran3.jl
@@ -2,11 +2,11 @@
 export libarpack32
 
 using CompilerSupportLibraries_jll
-using OpenBLAS32_jll
+using libblastrampoline_jll
 JLLWrappers.@generate_wrapper_header("Arpack32")
 JLLWrappers.@declare_library_product(libarpack32, "libarpack.so.2")
 function __init__()
-    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, OpenBLAS32_jll)
+    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, libblastrampoline_jll)
     JLLWrappers.@init_library_product(
         libarpack32,
         "lib/libarpack.so",
